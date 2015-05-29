2e311d0a45f70cf7fc691ebbdd1ef0ab1f79c465
KiresMA <kires.ahsim@inbox.ru>
2015-05-30 00:23:02 +0200

    Change costbills description length

--- migrations/m150409_162807_create_costsBills_table.php
+++ migrations/m150409_162807_create_costsBills_table.php
@@ -12,7 +12,7 @@ class m150409_162807_create_costsBills_table extends Migration
             'costsBillDate' => Schema::TYPE_DATE . ' NOT NULL',
             'costsBillNo' => Schema::TYPE_STRING . '(45) NOT NULL',
             'costsBillValue' => Schema::TYPE_STRING . '(45) NOT NULL',
-            'costsBillDescription' => Schema::TYPE_STRING . '(45) NOT NULL',
+            'costsBillDescription' => Schema::TYPE_STRING . '(255) NOT NULL',
             'offers_offerId' => Schema::TYPE_INTEGER . ' NOT NULL',
             'contractors_contractorId' => Schema::TYPE_INTEGER . ' NOT NULL'
         ]);
