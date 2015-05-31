4065958677d627f5f7f826872c8b460b6f0b32bf
Ashkro <ashkroo@gmail.com>
2015-05-31 17:36:51 +0200

    comment

--- controllers/OfferController.php
+++ controllers/OfferController.php
@@ -62,7 +62,7 @@ public function beforeAction($action)
      */
     public function actionList()
     {   
-    	if (Yii::$app->user->isGuest || Yii::$app->user->identity->isCustomer ()) {
+    	/*if (Yii::$app->user->isGuest || Yii::$app->user->identity->isCustomer ()) {
     		$query = Offer::find();
     		
     		$pagination = new Pagination([
@@ -79,7 +79,7 @@ public function beforeAction($action)
     				'offers' => $offers,
     				'pagination' => $pagination,
     		]);
-    	}
+    	}*/
     	
         $searchModel = new OfferSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
